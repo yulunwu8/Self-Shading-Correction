a71f087d82040af66084b666d4b3992670a0f064
yulunmaps <100971599+yulunwu8@users.noreply.github.com>
2025-08-05 14:06:18 -0400

    update cone size

--- tests/test.py
+++ tests/test.py
@@ -14,6 +14,6 @@ file_in = 'Rw_shaded.csv'
 file_out = 'Rw_corrected.csv'
 start_column = 10
 sza_column = 'sza'
-radius = 0.05 # in m
+radius = 0.038 # in m
 
 ssc.run(file_in, file_out, start_column, sza_column, radius)
